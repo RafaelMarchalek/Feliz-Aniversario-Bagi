62611901cf22520cb9ad551f9e119bbee1cec87f
RafaelVicenteMarchalek <dragaodegalocha@gmail.com>
2026-03-01 22:41:43 -0300

    add foto, e mudanças simples

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ document.addEventListener("click", function tocarUmaVez() {
     musica.play();
     document.removeEventListener("click", tocarUmaVez);
 });
+
+
+function criarConfete() {
+    const confete = document.createElement("div");
+    confete.classList.add("confetti");
+
+    confete.style.left = Math.random() * 100 + "vw";
+    confete.style.backgroundColor = coresAleatorias();
+    confete.style.animationDuration = (Math.random() * 3 + 2) + "s";
+
+    document.body.appendChild(confete);
+
+    setTimeout(() => {
+        confete.remove();
+    }, 5000);
+}
+
+function coresAleatorias() {
+    const cores = ["#ffffff", "#ff4e8a", "#ffd700", "#00ffff", "#ff7eb3"];
+    return cores[Math.floor(Math.random() * cores.length)];
+}
+
+setInterval(criarConfete, 200);
